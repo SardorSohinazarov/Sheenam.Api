dd403c837db9ec875f8e8feb9da232946182a680
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-25 22:28:14 +0500

    INFRA : API Project Init

---
+++ Sheenam/Sheenam/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by me :)
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public IActionResult GetAction() => Ok("Sardor yoratta");
+    }
+}
