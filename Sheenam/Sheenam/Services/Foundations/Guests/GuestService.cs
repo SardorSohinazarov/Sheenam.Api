5078c3c6adbd5d05528fdd75a70658a9b7dc184a
Sardor Sohinazarov <sardorstudent0618@gmail.com>
2023-02-28 02:48:11 +0500

    CODE RUB: cleaned my code

---
+++ Sheenam/Sheenam/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Sheenam.Api.Broker.StorageBroker;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IStorageBroker _storageBroker;
+
+        public GuestService(IStorageBroker storageBroker)=>
+            _storageBroker = storageBroker;
+
+        public ValueTask<Guest> AddGuestAsync(Guest guest)=>
+            throw new System.NotImplementedException();
+    }
+}
